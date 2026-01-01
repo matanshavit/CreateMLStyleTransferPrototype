fa47318d29a58ab012420b3d6f151660623b9244
Matan Shavit <71092861+matanshavit@users.noreply.github.com>
2026-01-01 17:05:48 -0500

    Initial commit: CreateML Style Transfer Prototype project setup

---
+++ CreateMLStyleTransferPrototype/CreateMLStyleTransferPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateMLStyleTransferPrototypeApp.swift
+//  CreateMLStyleTransferPrototype
+//
+//  Created by Matan Shavit on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct CreateMLStyleTransferPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
